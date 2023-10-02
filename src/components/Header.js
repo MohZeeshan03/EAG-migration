08cd1ea5f18738eeafe44a67598d38e253a91f82
ashishbhungani97 <bhunganiashish@gmail.com>
2023-10-02 23:05:56 +0530

    A

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logoImg from '../assets/logo.png';
+import icon from '../assets/icon.png'
 import Connect from './ConnectButton';
 
 export default function Header() {
     return (
         <>
              <header className="sl-container flex justify-between items-center">
-                <a href="#sec" className="md:max-w-sm sm:max-w-[18rem] sm:py-8 py-6"><img src={logoImg} alt="" /></a>
+             <a href="" className="md:max-w-sm sm:max-w-[18rem] max-w-[5rem] sm:py-8 py-6">
+                <img className="sm:block hidden" src={logoImg} alt="" />
+             <img className="sm:hidden h-12" src={icon} alt="" /></a>
                 <Connect/>
             </header>
         </>
