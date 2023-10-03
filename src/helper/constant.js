42ebc0747a44482736526ce0d7a727c8c95ee0aa
Mohammad Zeeshan <104795669+MohZeeshan03@users.noreply.github.com>
2023-10-03 15:38:39 +0500

    Update constant.js

--- src/helper/constant.js
+++ src/helper/constant.js
@@ -1,22 +1,22 @@
 export const PROJECT_ID = '27b9720018bf7f8740936e6a6eb28604';
 export const NETWORKS = {
  
-  5: {
-    name: "Testnet ETH",
-    chainId: 5,
-    rpc: "https://rpc.ankr.com/eth_goerli",
+  1: {
+    name: "Ethereum Mainnet",
+    chainId: 1,
+    rpc: "https://rpc.ankr.com/eth",
     symbol: 'ETH',
-    MIGRATION_ADDRESS: "0x3c948aCf79D845BA341D9410dD0FB9CF43E888e7",
-    GLDN_ADDRESS : "0x16eadee76a7c11820fe5640a0dabfaf9af41e9a4",
-    EAG_ADDRESS : "0x0788d2cd1bfc8189f3d43845a7e6b96118478011",
+    MIGRATION_ADDRESS: "0xcCE4417228dD48561cfBdfce3B825581606B70d0",
+    GLDN_ADDRESS : "0xFeeB4D0f5463B1b04351823C246bdB84c4320CC2",
+    EAG_ADDRESS : "0xf3B1a5292848431006cA28E2dEfF4FA78Fd928E4",
     MULTICALL_ADDRESS: "0xA78B1c073483655DC5eCf568CFef12Ab9b30338C",
    
   }
 }
 
-export const DEFAULT_CHAIN = 5;
-export const ADDREXPLORER = "https://goerli.etherscan.io/address/";
-export const TXEXPLORER = "https://goerli.etherscan.io/tx/";
+export const DEFAULT_CHAIN = 1;
+export const ADDREXPLORER = "https://etherscan.io/address/";
+export const TXEXPLORER = "https://etherscan.io/tx/";
 export const GLDN_SYMBOL = "GLDN";
 export const EAG_SYMBOL = "EAG";
 export const GLDN_DECIMALS = 18;
