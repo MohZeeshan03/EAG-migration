a3534a44fd4116f10205d85c952e25b14f5bad80
ashishbhungani97 <bhunganiashish@gmail.com>
2023-10-03 16:37:26 +0530

    A

--- src/stats/useGldn.js
+++ src/stats/useGldn.js
@@ -30,8 +30,8 @@ export const useGldnStats = (updater, lockId) => {
                 const data = await getMultiCall(
                     address ? [
                         migrationContract.methods.claimAvailable(),
-                        gldnContract.methods.balanceOf("0xc31328e68bA9770D4C3375FD2B7c79C9904c711f"),
-                        gldnContract.methods.allowance("0xc31328e68bA9770D4C3375FD2B7c79C9904c711f", NETWORKS[DEFAULT_CHAIN].MIGRATION_ADDRESS),
+                        gldnContract.methods.balanceOf(address),
+                        gldnContract.methods.allowance(address, NETWORKS[DEFAULT_CHAIN].MIGRATION_ADDRESS),
                     ] : [
                         migrationContract.methods.claimAvailable()
                     ]);
