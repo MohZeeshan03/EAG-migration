64cd74c321eba9c23d92578d6497c52015962580
Mohammad Zeeshan <104795669+MohZeeshan03@users.noreply.github.com>
2023-10-03 15:40:16 +0500

    Update wagmi.js

--- src/helper/wagmi.js
+++ src/helper/wagmi.js
@@ -1,9 +1,9 @@
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { configureChains, createConfig } from 'wagmi'
-import { goerli } from 'wagmi/chains'
+import { mainnet } from 'wagmi/chains'
 import { PROJECT_ID } from './constant'
 
-const chains = [goerli];
+const chains = [mainnet];
 
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId : PROJECT_ID })])
